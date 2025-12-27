465d343e205464b045d3a003a5390214b581e67a
hannws <hanws1219@gmail.com>
2025-12-27 22:45:39 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -1,10 +1,11 @@
 size = int(input())
-s = []
-for _ in range(size):
-    s.append(input())
-result = s[0]
+s = [input() for _ in range(size)]
+
+result = list(s[0])
+
 for i in range(1, size):
-    for j in range(len(s[i])):
-        if result[j] != s[i][j]:
-            result = result[:j] + "?" + result[j+1:]
-print(result)
+    for j in range(len(result)):
+        if result[j] != '?' and result[j] != s[i][j]:
+            result[j] = '?'
+
+print(''.join(result))
