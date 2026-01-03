e5e0b02cfbd811a867bfb0a355e4ccb342770e91
hannws <hanws1219@gmail.com>
2026-01-03 10:13:58 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 504 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,10 @@
+numbers, result = list(map(int, input().split())), 1
+while True:
+    cnt = 0
+    for i in numbers:
+        if result % i == 0:
+            cnt += 1
+    if cnt >=3:
+        print(result)
+        break
+    result += 1
