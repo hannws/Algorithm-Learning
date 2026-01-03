826237ca3b87b4c2d90087431f9f6307d8a954ac
hannws <hanws1219@gmail.com>
2026-01-03 11:03:01 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -2,7 +2,7 @@ size = int(input())
 for _ in range(size):
     cost = int(input())
     Quarter = cost // 25
-    Dime = (cost - 25*Quarter) // 10
-    Nickel = (cost - 25*Quarter - 10*Dime)//5
-    Penny = (cost - 25*Quarter - 10*Dime - 5*Nickel)//1
+    Dime = (cost%25) // 10
+    Nickel = ((cost%25)%10)//5
+    Penny = (cost%5)
     print(Quarter, Dime, Nickel, Penny)
