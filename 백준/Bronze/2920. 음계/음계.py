415e1b59c2954b7cfe37338d791a3811899b71a4
hannws <hanws1219@gmail.com>
2026-01-06 21:53:15 +0900

    [Bronze II] Title: 음계, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,10 @@
+import sys
+nums = list(map(int, sys.stdin.readline().split()))
+ascend = [1,2,3,4,5,6,7,8]
+descend = [8,7,6,5,4,3,2,1]
+if nums == ascend:
+    print("ascending")
+elif nums == descend:
+    print("descending")
+else:
+    print("mixed")
