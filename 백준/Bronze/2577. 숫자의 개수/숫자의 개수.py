d776d9ed71b61f5ee546de59228eb1e1a4f55bca
hannws <hanws1219@gmail.com>
2026-01-06 22:02:32 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,9 +1,6 @@
 import sys
 
 nums = [int(sys.stdin.readline()) for _ in range(3)]
-test = [0,1,2,3,4,5,6,7,8,9]
-cnt =[0,0,0,0,0,0,0,0,0,0]
 result = str(nums[0]*nums[1]*nums[2])
-for i in test:
-    cnt[i] = result.count(f"{i}")
-print('\n'.join(map(str, cnt)))
+for i in range(10):
+    print(result.count(str(i)))
