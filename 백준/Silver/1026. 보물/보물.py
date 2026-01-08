4aaa4deef393b612d747d9f93bd0bc0779bb9d24
hannws <hanws1219@gmail.com>
2026-01-08 19:44:12 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+A.sort(reverse=True)
+B.sort()
+result = 0
+for i in range(N):
+    result += A[i]*B[i]
+print(result)
