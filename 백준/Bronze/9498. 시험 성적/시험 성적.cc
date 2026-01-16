434aa8da00ffb3cb4541cf76a1aa64ab7f2b35f4
hannws <hanws1219@gmail.com>
2026-01-16 21:31:04 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N;
+	cin >> N;
+	if (N >= 90 && N <= 100) {
+		cout << 'A';
+	}
+	else if (N >= 80 && N < 90) {
+		cout << 'B';
+	}
+	else if (N >= 70 && N < 80) {
+		cout << 'C';
+	}
+	else if (N >= 60 && N < 70) {
+		cout << 'D';
+	}
+	else {
+		cout << 'F';
+	}
+	return 0;
+}
