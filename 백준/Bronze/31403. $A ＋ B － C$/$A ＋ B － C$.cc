515307bb01f3ba110d716dd07e7ac4df6bdf9980
hannws <hanws1219@gmail.com>
2026-01-18 15:09:50 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int a, b, c;
+	cin >> a >> b >> c;
+	string s = to_string(a) + to_string(b);
+
+	cout << (a + b - c) << "\n";
+	cout << stoi(s) - c << '\n';
+
+	return 0;
+}
