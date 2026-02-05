ee12b16f52d3bb3936fedfcc6e85fe0a4cdd2a6b
hannws <hanws1219@gmail.com>
2026-02-05 15:35:52 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string alpa;
+	cin >> alpa;
+
+	cout << alpa.size() << endl;
+	return 0;
+}
