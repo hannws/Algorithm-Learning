081e7031d36e69d2ad6ab576a069484468f5a7d4
hannws <hanws1219@gmail.com>
2026-02-07 21:08:23 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 112 ms, Memory: 40328 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -6,36 +6,25 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 
 def bfs(start, end):
-
-    if start == end:
-        return (0, 1)
-
     dis = [-1]*100001
+    ways = [0]*100001
+
     dis[start] = 0
-    dq = deque()
-    dq.append(start)
-    stopsign = inf
-    cnt = 0
+    ways[start] = 1
+    dq = deque([start])
 
     while dq:
-        idx = dq.popleft()
+        x = dq.popleft()
 
-        if dis[idx] > stopsign:
-            continue
-        elif dis[idx] == stopsign:
-            if idx == end:
-                cnt += 1
-        else:
-            for nxt in (idx+1, idx-1, idx*2):
-                if 0<= nxt <= 100000:
-                    if dis[nxt] == -1:
-                        dis[nxt] = dis[idx] + 1
-                        dq.append(nxt)
-                        if nxt == end:
-                            stopsign = dis[nxt]
-                    elif dis[nxt] == dis[idx] + 1:
-                        dq.append(nxt)
-    return (stopsign, cnt)
+        for nx in (x-1, x+1, x*2):
+            if 0 <= nx <= 100000:
+                if dis[nx] == -1:
+                    dis[nx] = dis[x] + 1
+                    ways[nx] = ways[x]
+                    dq.append(nx)
+                elif dis[nx] == dis[x] + 1:
+                    ways[nx] += ways[x]
+    return dis[end], ways[end]
 
 t, path = bfs(N, K)
 print(t)
