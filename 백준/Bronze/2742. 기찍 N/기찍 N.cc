4863daabeff140ff4aa47c4ce019f80d46a28ac6
hannws <hanws1219@gmail.com>
2026-02-12 15:00:13 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+	do {
+		cout << N << '\n';
+	} while (--N);
+	return 0;
+}
