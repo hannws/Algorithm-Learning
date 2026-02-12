7de648d42904eaa9c7e600e2bee3a182db8ddb30
hannws <hanws1219@gmail.com>
2026-02-12 17:53:26 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int N;
+	cin >> N;
+	while (N--)
+		cout << "I love DGU" << '\n';
+	return 0;
+}
