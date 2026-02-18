824e8c5e974d7b5be76a6388b55a486a0a1db7ce
hannws <hanws1219@gmail.com>
2026-02-18 20:58:45 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 100 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+def phi(x):
+    result = x
+    p = 2
+    
+    while p * p <= x:
+        if x % p == 0:
+            while x % p == 0:
+                x //= p
+            result -= result // p
+        p += 1
+    
+    if x > 1:
+        result -= result // x
+    
+    return result
+
+print(phi(n))
