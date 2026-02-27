ef9a19ad55b95a3713e1b375551d62b833e0a411
hannws <hanws1219@gmail.com>
2026-02-27 23:47:15 +0900

    [Silver II] Title: A → B, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,18 +1,19 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 A, B = map(int, input().split())
-dq = deque()
-dq.append((0, A))
+cnt = 1
 
-while dq:
-    cost, value = dq.popleft()
-    
-    for i in (value*10+1, value*2):
-        if i < B:
-            dq.append((cost+1, i))
-        elif i==B:
-            print(cost+2)
-            sys.exit(0)
-print(-1)
+while B > A:
+    if B%10 == 1:
+        B //= 10
+    elif B%2 == 0:
+        B //=2
+    else:
+        break
+    cnt += 1
+
+if B==A:
+    print(cnt)
+else:
+    print(-1)
