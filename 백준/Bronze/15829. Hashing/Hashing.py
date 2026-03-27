ceeaac17e2bdf00f975e61a731924941107dcb16
hannws <hanws1219@gmail.com>
2026-03-27 14:26:33 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,9 +3,14 @@ input = sys.stdin.readline
 
 n = int(input())
 s = input().rstrip()
+
+M = 1234567891
 total = 0
+r = 1
 
 for i in range(n):
-    total += (ord(s[i]) - ord('a') + 1)*(31**i)
+    value = ord(s[i]) - ord('a') +1
+    total = (total + value*r)%M
+    r = (r*31)%M
 
 print(total)
